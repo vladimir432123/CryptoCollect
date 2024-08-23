e2325bc4ed8059baa81fc1d367351985e77348b6
vladimir432123 <vladimirnomer4@gmail.com>
2024-08-23 20:12:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
-// src/main.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ProgressProvider } from './ProgressContext';
 import { GlobalStateProvider } from './GlobalState';
+import { BrowserRouter } from 'react-router-dom'; // Импортируем BrowserRouter
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // Create a root.
 
 root.render(
   <React.StrictMode>
-    <GlobalStateProvider>
-      <ProgressProvider>
-        <App />
-      </ProgressProvider>
-    </GlobalStateProvider>
+    <BrowserRouter> {/* Оборачиваем приложение в BrowserRouter */}
+      <GlobalStateProvider>
+        <ProgressProvider>
+          <App />
+        </ProgressProvider>
+      </GlobalStateProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
