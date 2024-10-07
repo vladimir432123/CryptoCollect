ab357c80dd02d6ad836c823d4efa9b57056d63ef
vladimir432123 <vladimirnomer4@gmail.com>
2024-10-07 20:46:54 +0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -194,7 +194,7 @@ bot.start(async (ctx) => {
     }
 
     // Заменяем кнопку с ссылкой на кнопку web_app
-    const webAppUrl = `https://app-21c4d0cd-2996-4394-bf8a-a453b9f7e396.cleverapps.io/?userId=${telegramId}&token=${sessionToken}`;
+    const webAppUrl = `https://cryptocollect.onrender.com/?userId=${telegramId}&token=${sessionToken}`;
 
     await ctx.reply(
       'Добро пожаловать! Нажмите на кнопку ниже, чтобы открыть приложение:',
